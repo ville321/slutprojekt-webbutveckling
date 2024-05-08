a3f0a6d2424d6ea865b9098a355e6b3ac5b020bb
ville321 <ville435@outlook.com>
2024-05-08 10:54:54 +0200

    bilder

---
+++ script/script.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        document.querySelector(".navbar").classList.add("sticky");
+    } else {
+        document.querySelector(".navbar").classList.remove("sticky");
+    }
+}
