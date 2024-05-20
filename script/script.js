cd3dac090c1f5d2b062d0d353882693224f03081
ville321 <ville435@outlook.com>
2024-05-20 23:56:17 +0200

    gdvzs

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
-window.onscroll = function() {
+window.onscroll = function () {
     if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
         document.querySelector(".navbar").classList.add("sticky");
     } else {
         document.querySelector(".navbar").classList.remove("sticky");
     }
+}
+
+function BookConfirmation() {
+    alert("Du har bokat bord.")
 }
